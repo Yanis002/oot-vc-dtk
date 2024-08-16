e3b592c141ca06b5b107b9528614c8ea545466d4
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2024-08-16 04:44:36 +0200

    add missing tools

---
+++ diff_settings.py
@@ -0,0 +1,13 @@
+def add_custom_arguments(parser):
+    parser.add_argument("-v", "--version", help="Emulator version to diff", default="ce-j")
+
+def apply(config, args):
+    version = args.version
+    config["make_command"] = ["ninja"]
+    config["mapfile"] = f"build/{version}/oot-gc.elf.MAP"
+    config["source_directories"] = ["src", "include"]
+    config["arch"] = "ppc"
+    config["map_format"] = "mw" # gnu, mw, ms
+    config["build_dir"] = f"build/{version}/src" # only needed for mw and ms map formats
+    config["objdump_executable"] = "build/binutils/powerpc-eabi-objdump"
+    config["show_line_numbers_default"] = True
